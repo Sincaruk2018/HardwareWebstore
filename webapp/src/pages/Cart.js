832f3b23fa8537acf57cff4efffd41a238673cb9
Mauwmau <maurilio.meireles@gmail.com>
2021-07-27 00:23:14 -0300

    Incomplete cart with localstorage

--- webapp/src/pages/Cart.js
+++ webapp/src/pages/Cart.js
@@ -1,106 +1,40 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { Link } from "react-router-dom";
 
 import "../styles/Cart.css";
 
+import CartItem from "../components/CartItem";
+
 function Cart() {
 
-  const placaDeVideo = "images/placadevideo.jpg";
+  const [items, setItems] = useState([]);
+
+  // const [total, setTotal] = useState(0);
+  useEffect(() => {
+    const storageContent = localStorage.getItem("cart");
+    if (storageContent === null) {return;}
+    const cartItens = JSON.parse(storageContent);
+
+    setItems(cartItens);
+  }, []);
 
   return (
-      <main id="cart-panel">
+    <main id="cart-panel">
+      <div id="cart-head">
+        <span>TOTAL</span>
+        <span>$800</span>
+      </div>
 
-        <div id="cart-head">
-          <span>TOTAL</span>
-          <span>$800</span>
-        </div>
+      <div id="cart-body">
+        {items.map((current) =>
+          <CartItem image={current.thumbnail} title={current.name} price={current.price} />
+        )}
+      </div>
 
-        <div id="cart-body">
-
-          <div className="cart-item">
-            <img className="cart-image" src={placaDeVideo} alt={""} />
-
-            <div className="item-infos">
-              <span className="item-title">
-                Placa de vídeo Nvidia 700 Series 2GB
-              </span>
-              <span className="item-price">$200</span>
-              <div className="quantity-control">
-                  <button className="control-btn" >&minus;</button>
-                  <span className="centered">01</span>
-                  <button className="control-btn" >&#43;</button>
-              </div>
-            </div>
-
-            <div className="cart-infos">
-              <button className="remove-btn">Remover do Carrinho</button>
-              <div className="item-subtotal">
-                <span>Subtotal do Produto</span>
-                <span className="item-subtotal-valor">$200</span>
-              </div>
-            </div>
-
-          </div>
-
-          <div className="cart-item">
-            <img className="cart-image" src={placaDeVideo} alt={""} />
-
-            <div className="item-infos">
-              <span className="item-title">
-                Placa de vídeo Nvidia 700 Series 2GB
-              </span>
-              <span className="item-price">$200</span>
-              <div className="quantity-control">
-                  <button className="control-btn" >&minus;</button>
-                  <span className="centered">01</span>
-                  <button className="control-btn" >&#43;</button>
-              </div>
-            </div>
-
-            <div className="cart-infos">
-              <button className="remove-btn">Remover do Carrinho</button>
-              <div className="item-subtotal">
-                <span>Subtotal do Produto</span>
-                <span className="item-subtotal-valor">$200</span>
-              </div>
-            </div>
-
-          </div>
-
-          <div className="cart-item">
-            <img className="cart-image" src={placaDeVideo} alt={""} />
-
-            <div className="item-infos">
-              <span className="item-title">
-                Placa de vídeo Nvidia 700 Series 2GB
-              </span>
-              <span className="item-price">$200</span>
-              <div className="quantity-control">
-                  <button className="control-btn" >&minus;</button>
-                  <span className="centered">01</span>
-                  <button className="control-btn" >&#43;</button>
-              </div>
-            </div>
-
-            <div className="cart-infos">
-              <button className="remove-btn">Remover do Carrinho</button>
-              <div className="item-subtotal">
-                <span>Subtotal do Produto</span>
-                <span className="item-subtotal-valor">$200</span>
-              </div>
-            </div>
-
-          </div>
-
-        </div>
-
-        <div id="cart-end" className="centered">
-          <Link to="/payment">
-            Finalizar Compra
-          </Link>
-        </div>
-
-      </main>
+      <div id="cart-end" className="centered">
+        <Link to="/payment">Finalizar Compra</Link>
+      </div>
+    </main>
   );
 }
 
