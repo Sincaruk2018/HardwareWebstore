22ca3f2ac7b30c5ff0e3111c2fd21246ca6ec8ea
Mauwmau <maurilio.meireles@gmail.com>
2021-07-28 16:36:23 -0300

    started server

---
+++ server/src/models/product.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const schema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    type: {
+        type: String,
+        trim: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    stock: {
+        type: Number,
+        required: true,
+    },
+});
+
+module.exports = mongoose.model('Product', schema);
